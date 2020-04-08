4b6a58a79645a43077fd982b01097d32b8598c5d
Ayush Mehar <ayushm19100@iiitnr.edu.in>
2020-04-08 17:17:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mehar/timestable/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.mehar.timestable;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @TargetApi(Build.VERSION_CODES.O)
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SeekBar seekBar = (SeekBar)(findViewById(R.id.seekBar));
+        seekBar.setMax(100);
+        seekBar.setMin(1);
+        seekBar.setProgress(1);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Log.i("Seekbar Changed",Integer.toString(i));
+                ArrayList<Integer> nums = new ArrayList<>();
+                ListView listView = (ListView)(findViewById(R.id.tables));
+                for(int j=1;j<=10;j++){
+                    nums.add(j*i);
+                }
+                ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_list_item_1,nums);
+                listView.setAdapter(arrayAdapter);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+}
